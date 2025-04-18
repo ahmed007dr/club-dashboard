f5edbdf6b75d0e9d3464eb19ece17d26841600f1
ahmed <ahmed007eg@gmail.com>
2025-04-18 02:27:47 +0200

    create فtickets app with models

---
+++ tickets/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Ticket(models.Model):
+    club = models.ForeignKey('core.Club', on_delete=models.CASCADE)
+    buyer_name = models.CharField(max_length=255)
+    ticket_type = models.CharField(max_length=50, choices=[('day_pass', 'Day Pass'), ('session', 'Session')])
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    used = models.BooleanField(default=False)
+    issue_date = models.DateField(auto_now_add=True)
+    used_by = models.ForeignKey('members.Member', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.ticket_type} - {self.buyer_name}"
+
+
