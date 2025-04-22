1eb2325a0ef22e5158483af794543f8eeb1420b6
Hananem <hananemai1995@gmail.com>
2025-04-22 19:09:05 +0200

    re

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:8000/api/auth/login', {
+      const response = await axios.post('http://127.0.0.1:8000/api/login', {
         email,
         password
       });
