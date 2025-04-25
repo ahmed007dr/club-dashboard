45ebcc43aeca8f922403098ad681fbd23db9ad36
ahmed <ahmed007eg@gmail.com>
2025-04-25 18:43:05 +0300

    full api postman test

--- core/api.py
+++ core/api.py
@@ -6,10 +6,12 @@ from rest_framework.response import Response
 from .models import Club
 from .serializers import ClubSerializer
 from .forms import ClubForm
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET'])
-@login_required
+@permission_classes([IsAuthenticated])
 def api_club_profile(request):
     club = get_object_or_404(Club, id=request.user.club_id)
     serializer = ClubSerializer(club)
@@ -17,7 +19,7 @@ def api_club_profile(request):
 
 
 @api_view(['POST'])
-@login_required
+@permission_classes([IsAuthenticated])
 def api_edit_club(request):
     club = get_object_or_404(Club, id=request.user.club_id)
     serializer = ClubSerializer(club, data=request.data, partial=True)
