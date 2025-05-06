18560fcd6a4670395c60f50ea7234e0d484afd8f
ahmed <ahmed007eg@gmail.com>
2025-05-06 21:11:53 +0300

    add rfid

--- members/serializers.py
+++ members/serializers.py
@@ -4,7 +4,7 @@ from .models import Member
 class MemberSerializer(serializers.ModelSerializer):
     referred_by_name = serializers.CharField(source='referred_by.name', read_only=True)
     club_name = serializers.CharField(source='club.name', read_only=True)
-    
+
     class Meta:
         model = Member
         fields = [
@@ -13,15 +13,25 @@ class MemberSerializer(serializers.ModelSerializer):
             'club_name',
             'name',
             'membership_number',
+            'rfid_code',
             'national_id',
             'birth_date',
             'phone',
+            'phone2',
             'photo',
+            'job',
+            'address',
+            'note',
             'created_at',
             'referred_by',
             'referred_by_name'
         ]
         extra_kwargs = {
             'photo': {'required': False, 'allow_null': True},
-            'referred_by': {'required': False, 'allow_null': True}
+            'referred_by': {'required': False, 'allow_null': True},
+            'rfid_code': {'required': False, 'allow_null': True},
+            'phone2': {'required': False, 'allow_null': True},
+            'job': {'required': False, 'allow_null': True},
+            'address': {'required': False, 'allow_null': True},
+            'note': {'required': False, 'allow_null': True},
         }
