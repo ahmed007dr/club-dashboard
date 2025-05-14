e445fa654d8e601746d650da5970c3ecb614bc4b
ahmed <ahmed007eg@gmail.com>
2025-05-14 14:46:01 +0300

    logger

--- project/settings.py
+++ project/settings.py
@@ -190,38 +190,49 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
+# Basic settings
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Logging Configuration
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {asctime} {message}',
             'style': '{',
         },
     },
     'handlers': {
         'file': {
-            'level': 'INFO',
+            'level': 'DEBUG', 
             'class': 'logging.FileHandler',
-            'filename': 'devices.log',
+            'filename': os.path.join(BASE_DIR, 'debug.log'), 
             'formatter': 'verbose',
         },
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
+            'formatter': 'simple',
         },
     },
     'loggers': {
-        'devices': {
+        '': { 
             'handlers': ['file', 'console'],
-            'level': 'INFO',
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': { 
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
             'propagate': False,
         },
     },
-}
-# Static files (CSS, JavaScript, Images)
+}# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/' 
