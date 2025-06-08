515b264794aad48b3f6938cf95b660388b3b5c65
ahmed <ahmed007eg@gmail.com>
2025-06-08 18:53:23 +0300

    update type ticket

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import api
 
+
 urlpatterns = [
     path('api/tickets/', api.ticket_list_api, name='api-ticket-list'),
     path('api/tickets/add/', api.add_ticket_api, name='api-add-ticket'),
     path('api/tickets/<int:ticket_id>/', api.ticket_detail_api, name='api-ticket-detail'),
     path('api/tickets/<int:ticket_id>/delete/', api.delete_ticket_api, name='api-delete-ticket'),
     path('api/ticket-types/', api.ticket_type_list_api, name='api-ticket-type-list'),
+    path('api/ticket-types/add/', api.add_ticket_type_api, name='api-add-ticket-type'), 
 ]
